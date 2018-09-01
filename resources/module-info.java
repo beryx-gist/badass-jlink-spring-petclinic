8f5e6ad3a0a0021823a53fb087ee96f6126dcab0
Serban Iordache <iordache@hotmail.com>
2018-09-01 22:44:15 +0200

    module-info cleanup

--- resources/module-info.java
+++ resources/module-info.java
@@ -2,7 +2,6 @@ open module org.springframework.samples.petclinic {
     requires java.xml.bind;
     requires java.management;
     requires java.transaction;
-
     requires java.activation;
     requires java.instrument;
     requires java.persistence;
@@ -10,7 +9,7 @@ open module org.springframework.samples.petclinic {
     requires java.validation;
     requires jdk.unsupported;
 
-//    requires hibernate.jpa;
+    requires cache.api;
     requires org.hibernate.validator;
 
     requires spring.beans;
@@ -23,13 +22,4 @@ open module org.springframework.samples.petclinic {
     requires spring.data.jpa;
     requires spring.boot;
     requires spring.boot.autoconfigure;
-    requires cache.api;
-/*
-    exports org.springframework.samples.petclinic;
-    exports org.springframework.samples.petclinic.model;
-    exports org.springframework.samples.petclinic.owner;
-    exports org.springframework.samples.petclinic.system;
-    exports org.springframework.samples.petclinic.vet;
-    exports org.springframework.samples.petclinic.visit;
-*/
 }
